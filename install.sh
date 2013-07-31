2a94f453c919189fd5ac0aef42d212efedcf035e
Kam Kuo <kam@cukerinteractive.com>
2013-07-31 01:19:03 -0700

    reinstated git submodule code to install.sh

--- install.sh
+++ install.sh
@@ -16,10 +16,10 @@ do
     link_file $i
 done
 
-#git submodule sync
-#git submodule init
-#git submodule update
-#git submodule foreach git pull origin master
-#git submodule foreach git submodule init
-#git submodule foreach git submodule update
+git submodule sync
+git submodule init
+git submodule update
+git submodule foreach git pull origin master
+git submodule foreach git submodule init
+git submodule foreach git submodule update
 
