d576a37bb4c0d466ab4c4318035b780acc50d816
Kam Kuo <kam@cukerinteractive.com>
2013-12-05 09:28:25 -0800

    added KeyRemap4MacBook settings file

---
+++ _configs/kr4mb-import.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+cli=/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli
+
+$cli set option.vimode_hjkl 1
+/bin/echo -n .
+$cli set passthrough.fnescape 1
+/bin/echo -n .
+$cli set private.ergonomics_warning_fndel_mode 1
+/bin/echo -n .
+$cli set private.ergonomics_warning_del_mode 1
+/bin/echo -n .
+$cli set private.simultaneouskeypresses_SD_MiddleClick 1
+/bin/echo -n .
+$cli set private.simultaneouskeypresses_DF_LeftClick 1
+/bin/echo -n .
+$cli set remap.eject2expose 1
+/bin/echo -n .
+$cli set private.simultaneouskeypresses_spaces_mode_QW 1
+/bin/echo -n .
+$cli set private.virtual_onehanded_SPACE_QWERTY 1
+/bin/echo -n .
+$cli set private.z_to_quick_symbols 1
+/bin/echo -n .
+$cli set private.holdtab_to_virtual_numpad 1
+/bin/echo -n .
+$cli set private.hyper_to_ergonomics_mode 1
+/bin/echo -n .
+$cli set private.simultaneouskeypresses_AS_RightClick 1
+/bin/echo -n .
+$cli set remap.optionR2controlR 1
+/bin/echo -n .
+$cli set private.PC_to_Hyper_plus_escape 1
+/bin/echo -n .
+$cli set remap.launchpad2dashboard 1
+/bin/echo -n .
+$cli set private.simultaneous_xc_to_virtual_numpad 1
+/bin/echo -n .
+$cli set private.simultaneouskeypresses_window_resizing_mode_WE 1
+/bin/echo -n .
+$cli set parameter.simultaneouskeypresses_delay 20
+/bin/echo -n .
+$cli set private.ergonomics_warning_mode 1
+/bin/echo -n .
+$cli set private.fn_sfhit_n_to_virtual_numlock 1
+/bin/echo -n .
+$cli set private.simultaneouskeypresses_virtual_cursorkeys_mode_CV 1
+/bin/echo -n .
+$cli set private.backslash_to_quick_symbols 1
+/bin/echo -n .
+$cli set private.fn_sfhit_c_to_virtual_cursorkeys_HJKL 1
+/bin/echo -n .
+$cli set private.F19_to_ergonomics_mode 1
+/bin/echo -n .
+$cli set remap.fnletter_to_ctrlletter 1
+/bin/echo -n .
+$cli set private.virtual_lefthanded_SPACE_QWERTY 1
+/bin/echo -n .
+$cli set private.x_to_injured_mode 1
+/bin/echo -n .
+$cli set private.virtual_lefthanded_space_v_enter 1
+/bin/echo -n .
+$cli set option.drop_slowexpose 1
+/bin/echo -n .
+/bin/echo
